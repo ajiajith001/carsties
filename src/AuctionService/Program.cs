214a6133db44fbe40ebbb660f1abeaa7ef64853f
Aji T.A <ajiajith001@gmail.com>
2025-04-19 21:11:57 +0530

    Auction.Service - Initial commit

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,26 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try {
+    DbInitializer.InitDb(app);
+} catch (Exception ex) {
+    Console.WriteLine($"Error: {ex.Message}");
+    throw;
+}
+
+app.Run();

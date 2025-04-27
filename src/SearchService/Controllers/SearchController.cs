51422345c161ebcb51f22c271c298a7b83f110c3
Aji T.A <ajiajith001@gmail.com>
2025-04-27 18:39:43 +0530

    SearchAPI - loading data from AuctionService through HTTP request

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] RequestParams searchParams)
+    {
+        var searchTerm = searchParams.SearchTerm;
+        var pageNumber = searchParams.PageNumber;
+        var pageSize = searchParams.PageSize;
+        var seller = searchParams.Seller;
+        var winner = searchParams.Winner;
+        var orderBy = searchParams.OrderBy;
+        var filter = searchParams.Filter;
+
+        var query = DB.PagedSearch<Item, Item>();
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        query = orderBy switch 
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+        };
+
+        query = filter switch 
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(seller)) {
+            query.Match(x => x.Seller == seller);
+        }
+
+        if (!string.IsNullOrEmpty(winner)) {
+            query.Match(x => x.Winner == winner);
+        }
+
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
+        var result = await query.ExecuteAsync();
+        return Ok(new {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount,
+        });
+    }
+}
